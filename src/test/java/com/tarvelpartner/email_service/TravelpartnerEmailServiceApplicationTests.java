423f5ac43412a5998fa3de3620d0fffb5c06be5b
srinivas <srinivas.sony777@gmail.com>
2024-09-26 22:46:11 +0530

    created the email service

---
+++ src/test/java/com/tarvelpartner/email_service/TravelpartnerEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarvelpartner.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelpartnerEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
