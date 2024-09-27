51a80b23a46d4c541400f0ec65551391d7caded3
srinivas <srinivas.sony777@gmail.com>
2024-09-27 22:50:41 +0530

    add loggers in app

--- src/main/java/com/travelpartner/email_service/service/UserEmailConsumer.java
+++ src/main/java/com/travelpartner/email_service/service/UserEmailConsumer.java
@@ -5,19 +5,31 @@ import java.util.Map;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import jakarta.annotation.PostConstruct;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class UserEmailConsumer {
 
-	@PostConstruct
-	public void init() {
-		System.out.println("UserEmailConsumer started...");
-	}
+	// Create a logger instance for logging
+	private static final Logger logger = LogManager.getLogger(UserEmailConsumer.class);
 
 	@KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
-	public void UserConsumer(Map<String, Object> message) {
-		System.out.println("message" + " " + message.get("name"));
+	public void UserInvitationEmailConsumer(Map<String, Object> message) {
+		try {
+			if (message != null && message.size()>0) {
+				String name = (String) message.get("name");
+				logger.info("Received message with user name: {}", name);
+
+				// Simulate message processing
+				logger.info("Processing user message for: {}", name);
+			} else {
+				logger.warn("Received invalid message: {}", message);
+			}
+		} catch (Exception e) {
+			// Log any error that occurs during message processing
+			logger.error("Error processing the Kafka message: {}", message, e);
+		}
 	}
 
 }
