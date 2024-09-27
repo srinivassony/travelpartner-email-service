7b651ca423f4056296bdbf28eb425ee5e1838c32
srinivas <srinivas.sony777@gmail.com>
2024-09-27 22:19:56 +0530

    add consumer class

--- src/main/java/com/tarvelpartner/email_service/TravelpartnerEmailServiceApplication.java
+++ src/main/java/com/tarvelpartner/email_service/TravelpartnerEmailServiceApplication.java
@@ -2,12 +2,16 @@ package com.tarvelpartner.email_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.travelpartner.email_service.service")
 public class TravelpartnerEmailServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelpartnerEmailServiceApplication.class, args);
+		
+		System.out.println("Application started!");
 	}
 
 }
--- src/main/java/com/travelpartner/email_service/service/UserEmailConsumer.java
+++ src/main/java/com/travelpartner/email_service/service/UserEmailConsumer.java
@@ -1,5 +1,23 @@
 package com.travelpartner.email_service.service;
 
+import java.util.Map;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
 public class UserEmailConsumer {
+
+	@PostConstruct
+	public void init() {
+		System.out.println("UserEmailConsumer started...");
+	}
+
+	@KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
+	public void UserConsumer(Map<String, Object> message) {
+		System.out.println("message" + " " + message.get("name"));
+	}
 
 }
