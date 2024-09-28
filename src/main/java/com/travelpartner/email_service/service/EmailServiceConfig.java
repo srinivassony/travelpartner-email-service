fb5c68ec29688b53444188d07718e22120040e5c
srinivas <srinivas.sony777@gmail.com>
2024-09-28 18:01:11 +0530

    consumer kafka configs

---
+++ src/main/java/com/travelpartner/email_service/service/EmailServiceConfig.java
@@ -0,0 +1,60 @@
+package com.travelpartner.email_service.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailServiceConfig {
+	
+	// Create a logger instance for logging
+		private static final Logger logger = LogManager.getLogger(EmailServiceConfig.class);
+
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	@Value("${spring.mail.username}")
+	private String sender;
+	
+	 public String sendNotification(String toEmailId,String subject,String message)
+	    {
+	 
+	        // Try block to check for exceptions
+	        try {
+	 
+	        	// Creating a MIME message
+	            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+	            
+	            // Creating a MimeMessageHelper to handle the message
+	            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+	            
+	            // Setting up necessary details
+	            messageHelper.setFrom(sender);
+	            messageHelper.setTo("srinivas.sony777@gmail.com");
+	            messageHelper.setSubject(subject);
+	            messageHelper.setText(message, true);  // Set 'true' to indicate HTML content
+
+	            // Sending the mail
+	            javaMailSender.send(mimeMessage);
+	            
+	            logger.info("Email input params: {}", mimeMessage);
+	            
+	            return "Mail Sent Successfully...";
+	        }
+	 
+	        // Catch block to handle the exceptions
+	        catch (Exception e) {
+	        	
+	        	logger.error("Error processing the email: {}",  e);
+	            return "Error while Sending Mail";
+	        }
+	    }
+
+}
--- src/main/java/com/travelpartner/email_service/service/UserEmailConsumer.java
+++ src/main/java/com/travelpartner/email_service/service/UserEmailConsumer.java
@@ -1,7 +1,9 @@
 package com.travelpartner.email_service.service;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +13,31 @@ import org.apache.logging.log4j.Logger;
 @Service
 public class UserEmailConsumer {
 
+	@Autowired
+	private EmailServiceConfig emailServiceConfig;
+
 	// Create a logger instance for logging
 	private static final Logger logger = LogManager.getLogger(UserEmailConsumer.class);
 
 	@KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
-	public void UserInvitationEmailConsumer(Map<String, Object> message) {
+	public void emailConsumer(Map<String, Object> message) {
 		try {
-			if (message != null && message.size()>0) {
-				String name = (String) message.get("name");
-				logger.info("Received message with user name: {}", name);
+			if (message != null && !message.isEmpty()) {
 
-				// Simulate message processing
-				logger.info("Processing user message for: {}", name);
+				logger.info("Received message: {}", message);
+
+				String toEmailId = (String) message.get("toEmailId");
+
+				String subject = (String) message.get("subject");
+
+				String content = (String) message.get("content");
+				
+				logger.info("input params: {}",  toEmailId, subject, content);
+
+				String emailResponse = emailServiceConfig.sendNotification(toEmailId, subject, content);
+
+				logger.info("Email response: {}", emailResponse);
+
 			} else {
 				logger.warn("Received invalid message: {}", message);
 			}
